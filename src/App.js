196b7ca28ee0c36f525da19b8e3943faca734b09
SolomonSitotaw23 <solomonsitotaw9@gmail.com>
2022-04-08 13:50:04 +0300

    header component and links

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShopPage from "./shop/shop.component";
-
+import Header from "./components/header/header.component";
 
 function App(props) {
   console.log(props);
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
-        
       </Routes>
     </Router>
   );

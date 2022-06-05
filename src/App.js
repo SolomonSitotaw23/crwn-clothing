4aad8234faedee0e74264f8973c03899da01351a
solomonsitotaw23 <solomonsitotaw9@gmail.com>
2022-06-05 16:06:59 +0300

    ablity to store authenticated user on firebase using firebase util

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShopPage from "./shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup.component";
-import { auth } from "./firebse/firebse.utils";
+import { auth, createUserProfileDocument } from "./firebse/firebse.utils";
 
 class App extends React.Component {
   constructor() {
@@ -16,9 +16,20 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
   componentWillUnmount() {
--- src/firebse/firebse.utils.js
+++ src/firebse/firebse.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const config = {
   apiKey: "AIzaSyA32EFLoEWXDWfY5v1xRZA67dLdviSzO4w",
@@ -11,9 +11,32 @@ const config = {
   appId: "1:254148751501:web:42bf909d8887628e0672c4",
   measurementId: "G-BRFP9M0G9F",
 };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating a user", error.message);
+    }
+  }
+  return userRef;
+};
 
 firebase.initializeApp(config);
-export const auth = firebase.auth(); 
+export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();

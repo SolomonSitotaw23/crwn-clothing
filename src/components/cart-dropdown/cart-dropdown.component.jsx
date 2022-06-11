219baecb7cb53c62a24a11716eb228bcfd989566
solomonsitotaw23 <solomonsitotaw9@gmail.com>
2022-06-11 14:19:00 +0300

    updating with reslect memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,19 +4,20 @@ import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 // redux
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropDown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-    {cartItems.map((cartItem) => (
-      <CartItem key={cartItem.id} item={cartItem}></CartItem>
-    ))}
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem}></CartItem>
+      ))}
     </div>
     <CustomButton> Go To Checkout</CustomButton>
   </div>
 );
-const mapStateTotProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateTotProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateTotProps)(CartDropDown);
